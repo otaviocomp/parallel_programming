91cb7a90535b134a7506d3bb2484a91f37ce2eb5
otaviocomp <otavio10espsanto@gmail.com>
2017-10-16 18:28:26 -0200

    openmp example

--- openmp/soma_omp.c
+++ openmp/soma_omp.c
@@ -5,18 +5,18 @@
 
 #define SIZE 10000000000
 
-int main()
+int main(int argc, char *argv[])
 {
 	clock_t t;
 	long int total = 0;
-	t = clock();
-	//int threads = strtol(argv[1], NULL, 10);
+	int threads = strtol(argv[1], NULL, 10);
 
-	#pragma omp parallel
+	t = clock();
+	#pragma omp parallel num_threads(threads)
 	{ 
+		long int i, soma = 0;
 		int rank = omp_get_thread_num();
 		int size = omp_get_num_threads();
-		long int i, soma = 0;
 		long int my_first = (SIZE/size)*rank;
 		long int my_last = my_first + SIZE/size;
 		for(i = my_first; i < my_last; i++)
@@ -24,6 +24,6 @@ int main()
 		#pragma omp critical
 			total += soma;	
 	}
-	t = clock()	- t;
+	t = (clock() - t)/threads;
 	printf("valor total = %ld\n executado no tempo = %f\n", total, (float)t/CLOCKS_PER_SEC);
 }	
