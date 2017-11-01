24f591c89b9357637275817477a61fc84b391d63
otaviocomp <otavio10espsanto@gmail.com>
2017-11-01 20:16:45 -0300

    update openmp

---
+++ openmp/sum.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+
+#define SIZE 10000000000
+
+int main(int arcv, char *argv[])
+{
+	clock_t t;
+	long int total = 0;
+	int size;
+	//int threads = strtol(argv[1], NULL, 10);
+
+	t = clock();
+	#pragma omp parallel
+	{
+		size = omp_get_num_threads();
+		long int i, soma = 0;
+		#pragma omp for
+		for(i = 0; i < SIZE; i++)
+			soma += i;
+		#pragma omp critical
+			total += soma;
+	}
+	t = (clock() - t)/size;
+	printf("valor total = %ld\nexecutado no tempo = %f\n", total, (float)t/CLOCKS_PER_SEC);
+}
+			
+
